bd4da53db7fcea1a7c5db33c0b1b0d4a9aee6d33
sameh <sameh080081@gmail.com>
2022-05-23 17:17:56 +0200

    add local storage.

--- index.js
+++ index.js
@@ -15,22 +15,50 @@ const form = document.forms['myform'];
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const bookName = form.querySelector('.title').value;
-    const bookTitle = form.querySelector('.author').value;
+    const bookTitle = form.querySelector('.title').value;
+    const bookAuthor = form.querySelector('.author').value;
+    //Store data
+    const data = JSON.stringify({bookTitle,bookAuthor});
+    localStorage.setItem('data',data);
     //creating elements
     const li = document.createElement('li');
     const author = document.createElement('span');
-    const title = document.createElement('span')
-    const btn = document.createElement('button')
+    const title = document.createElement('span');
+    const btn = document.createElement('button');
     btn.classList.add('remove');
     //appending
-    li.appendChild(author);
     li.appendChild(title);
+    li.appendChild(author);
     li.appendChild(btn);
-    bookList.appendChild(li)
+    bookList.appendChild(li);
     //Content display
     btn.textContent = 'Remove';
-    author.textContent = bookName;
+    author.textContent = bookAuthor;
+    title.textContent = bookTitle;
+});
+
+function populateStorage() {
+    const formValues = JSON.parse(localStorage.getItem('data'));
+    if (formValues) {
+    const bookTitle = formValues.bookTitle;
+    const bookAuthor = formValues.bookAuthor;
+
+    const li = document.createElement('li');
+    const author = document.createElement('span');
+    const title = document.createElement('span');
+    const btn = document.createElement('button');
+    btn.classList.add('remove');
+    //appending
+    li.appendChild(title);
+    li.appendChild(author);
+    li.appendChild(btn);
+    bookList.appendChild(li);
+    //Content display
+    btn.textContent = 'Remove';
+    author.textContent = bookAuthor;
     title.textContent = bookTitle;
 
-});
+    }
+  }
+
+  populateStorage();
