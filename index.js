cc3b8c71aa5bbb925cd8a31879ca551cf5ac3f55
afikanzoro <muchemimutahi@gmail.com>
2022-05-23 17:28:31 +0300

    Add javaScript code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const terminate = document.querySelectorAll('.remove');
 
+const terminate = document.querySelectorAll('.lists');
 Array.from(terminate).forEach((term) => {
     term.addEventListener('click', (e) => {
 
@@ -10,10 +10,27 @@ Array.from(terminate).forEach((term) => {
 });
 
 
-
+const bookList = document.querySelector('.lists')
 const form = document.forms['myform'];
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const bookName = form.querySelectorAll('input[type="text"]').value;
-})
+    const bookName = form.querySelector('.title').value;
+    const bookTitle = form.querySelector('.author').value;
+    //creating elements
+    const li = document.createElement('li');
+    const author = document.createElement('span');
+    const title = document.createElement('span')
+    const btn = document.createElement('button')
+    btn.classList.add('remove');
+    //appending
+    li.appendChild(author);
+    li.appendChild(title);
+    li.appendChild(btn);
+    bookList.appendChild(li)
+    //Content display
+    btn.textContent = 'Remove';
+    author.textContent = bookName;
+    title.textContent = bookTitle;
+
+});
