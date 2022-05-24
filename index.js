326733fc7dadb170a120fa80e45b10b190009e97
afikanzoro <muchemimutahi@gmail.com>
2022-05-24 12:33:38 +0300

    Fix linters and update Readme file

--- index.js
+++ index.js
@@ -1,55 +1,53 @@
-
 let data = JSON.parse(localStorage.getItem('data'));
-const books = document.getElementById('lists')
+const books = document.getElementById('lists');
 
-function displayBook(title,author){
-    const bookColumn = document.createElement('div');
-    {
-      const bookTitle = document.createElement('h3');
-      bookTitle.innerText = title;
-      bookColumn.appendChild(bookTitle);
-
-      const bookAuthor = document.createElement('h3');
-      bookAuthor.innerText = author;
-      bookColumn.appendChild(bookAuthor);
-
-      const removeBotton = document.createElement('button');
-      removeBotton.type = 'button';
-      removeBotton.innerText = 'Remove';
-      removeBotton.addEventListener('click', () => {
-        removeBook(title, author, bookColumn);
-      });
-      bookColumn.appendChild(removeBotton);
-
-      const vector = document.createElement('hr');
-      bookColumn.appendChild(vector);
+function removeBook(title, author, element) {
+  element.remove();
+  for (let i = 0; i < data.length; i += 1) {
+    if (data[i].title === title && data[i].author === author) {
+      data.splice(i, 1);
     }
-    books.appendChild(bookColumn);
+  }
+  localStorage.setItem('data', JSON.stringify(data));
 }
 
-function addBook(title,author){
-    data.push({title, author,});
-    localStorage.setItem('data', JSON.stringify(data));
-    displayBook(title,author);
+function displayBook(title, author) {
+  const bookColumn = document.createElement('div');
+  {
+    const bookTitle = document.createElement('h3');
+    bookTitle.innerText = title;
+    bookColumn.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement('h3');
+    bookAuthor.innerText = author;
+    bookColumn.appendChild(bookAuthor);
+
+    const removeBotton = document.createElement('button');
+    removeBotton.type = 'button';
+    removeBotton.innerText = 'Remove';
+    removeBotton.addEventListener('click', () => {
+      removeBook(title, author, bookColumn);
+    });
+    bookColumn.appendChild(removeBotton);
+
+    const vector = document.createElement('hr');
+    bookColumn.appendChild(vector);
+  }
+  books.appendChild(bookColumn);
 }
 
-function removeBook(title,author,element){
-    element.remove();
-    for (let i = 0; i < data.length; i += 1) {
-        if (data[i].title === title && data[i].author === author) {
-          data.splice(i, 1);
-        }
-      }
-    localStorage.setItem('data', JSON.stringify(data));
+function addBook(title, author) {
+  data.push({ title, author });
+  localStorage.setItem('data', JSON.stringify(data));
+  displayBook(title, author);
 }
 
-if(data) {
-    for (let i=0 ;i<data.length;i+=1){
-        displayBook(data[i].title,data[i].author);
-    }
-}
-else{
-    data=[];
+if (data) {
+  for (let i = 0; i < data.length; i += 1) {
+    displayBook(data[i].title, data[i].author);
+  }
+} else {
+  data = [];
 }
 
 const form = document.getElementById('myform');
@@ -57,8 +55,8 @@ const bookT = form.querySelector('.title');
 const bookA = form.querySelector('.author');
 
 form.addEventListener('submit', (e) => {
-    addBook(bookT.value,bookA.value);
-    bookT.value='';
-    bookA.value='';
-    e.preventDefault();
+  addBook(bookT.value, bookA.value);
+  bookT.value = '';
+  bookA.value = '';
+  e.preventDefault();
 });
