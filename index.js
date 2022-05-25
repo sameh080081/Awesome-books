73d86638ee21319a30beae519ffd69e8a9989993
afikanzoro <muchemimutahi@gmail.com>
2022-05-25 16:28:46 +0300

    Refactor JS to use classes & Add CSS

--- index.js
+++ index.js
@@ -1,62 +1,81 @@
-let data = JSON.parse(localStorage.getItem('data'));
-const books = document.getElementById('lists');
+class MyBooks {
+  constructor(_title, _author) {
+    this.title = _title;
+    this.author = _author;
+  }
+}
 
-function removeBook(title, author, element) {
-  element.remove();
-  for (let i = 0; i < data.length; i += 1) {
-    if (data[i].title === title && data[i].author === author) {
-      data.splice(i, 1);
+class Interface {
+  static displayBooks() {
+   const books = Storage.getBook();
+   books.forEach((book) => {
+     Interface.updateBookList(book)
+   }) 
+  }
+  static updateBookList(book) {
+    const list = document.querySelector('#lists')
+    const row = document.createElement('tr')
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>by</td>
+    <td>${book.author}</td>
+    <button class= 'delete' >Remove</button>
+    `;
+    list.appendChild(row);
+  }
+  static removeBook(el) {
+    if(el.classList.contains('delete')) {
+      el.parentElement.remove();
     }
+
   }
-  localStorage.setItem('data', JSON.stringify(data));
-}
-
-function displayBook(title, author) {
-  const bookColumn = document.createElement('div');
-  {
-    const bookTitle = document.createElement('h3');
-    bookTitle.innerText = title;
-    bookColumn.appendChild(bookTitle);
-
-    const bookAuthor = document.createElement('h3');
-    bookAuthor.innerText = author;
-    bookColumn.appendChild(bookAuthor);
-
-    const removeBotton = document.createElement('button');
-    removeBotton.type = 'button';
-    removeBotton.innerText = 'Remove';
-    removeBotton.addEventListener('click', () => {
-      removeBook(title, author, bookColumn);
-    });
-    bookColumn.appendChild(removeBotton);
-
-    const vector = document.createElement('hr');
-    bookColumn.appendChild(vector);
+  static clearInputs() {
+    document.querySelector('.title').value = '';
+    document.querySelector('.author').value = '';
   }
-  books.appendChild(bookColumn);
 }
 
-function addBook(title, author) {
-  data.push({ title, author });
-  localStorage.setItem('data', JSON.stringify(data));
-  displayBook(title, author);
-}
-
-if (data) {
-  for (let i = 0; i < data.length; i += 1) {
-    displayBook(data[i].title, data[i].author);
+class Storage {
+  static getBook() {
+    let books;
+    if(localStorage.getItem(books) === null) {
+      books = [];
+    }
+    else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
   }
-} else {
-  data = [];
+  static addBook(book) {
+    const books = Storage.getBook()
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+  static removeBook(author) {
+    const books = Storage.getBook();
+    books.forEach((book, index) => {
+      if(book.author === author) {
+        books.splice(index, 1);
+      }
+    })
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 }
 
-const form = document.getElementById('myform');
-const bookT = form.querySelector('.title');
-const bookA = form.querySelector('.author');
-
-form.addEventListener('submit', (e) => {
-  addBook(bookT.value, bookA.value);
-  bookT.value = '';
-  bookA.value = '';
+document.addEventListener('DOMContentLoaded', Interface.displayBooks())
+document.querySelector('#myform').addEventListener('submit', (e) => {
   e.preventDefault();
+  const title = document.querySelector('.title').value;
+  const author = document.querySelector('.author').value;
+  const book = new MyBooks(title, author);
+  console.log(book);
+  Interface.updateBookList(book);
+  Storage.addBook(book);
+  Interface.clearInputs();
+});
+
+const remove = document.querySelector('#lists');
+remove.addEventListener('click', (e) => {
+  Storage.removeBook(e.target.parentElement.previousElementSibling);
+  Interface.removeBook(e.target)
 });
