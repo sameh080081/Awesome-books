8de3829887de1eb9fdc0efe35979ce9c32646153
afikanzoro <muchemimutahi@gmail.com>
2022-05-25 18:14:03 +0300

    Fix Linters

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
+/* eslint max-classes-per-file: ["error", 3] */
+class Storage {
+  static getBook() {
+    let books;
+    if (JSON.parse(localStorage.getItem('books')) === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Storage.getBook();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(title, author) {
+    const books = Storage.getBook();
+    books.forEach((book, index) => {
+      if (book.title === title && book.author === author) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
 class MyBooks {
   constructor(_title, _author) {
     this.title = _title;
     this.author = _author;
   }
 }
-
 class Interface {
   static displayBooks() {
-   const books = Storage.getBook();
-   books.forEach((book) => {
-     Interface.updateBookList(book)
-   }) 
+    const books = Storage.getBook();
+    books.forEach((book) => {
+      Interface.updateBookList(book);
+    });
   }
+
   static updateBookList(book) {
-    const list = document.querySelector('#lists')
-    const row = document.createElement('tr')
+    const list = document.querySelector('#lists');
+    const row = document.createElement('tr');
     row.innerHTML = `
     <td>${book.title}</td>
     <td>by</td>
@@ -23,59 +52,34 @@ class Interface {
     `;
     list.appendChild(row);
   }
+
   static removeBook(el) {
-    if(el.classList.contains('delete')) {
+    if (el.classList.contains('delete')) {
       el.parentElement.remove();
     }
-
   }
+
   static clearInputs() {
     document.querySelector('.title').value = '';
     document.querySelector('.author').value = '';
   }
 }
 
-class Storage {
-  static getBook() {
-    let books;
-    if(localStorage.getItem(books) === null) {
-      books = [];
-    }
-    else {
-      books = JSON.parse(localStorage.getItem('books'));
-    }
-    return books;
-  }
-  static addBook(book) {
-    const books = Storage.getBook()
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-  static removeBook(author) {
-    const books = Storage.getBook();
-    books.forEach((book, index) => {
-      if(book.author === author) {
-        books.splice(index, 1);
-      }
-    })
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-}
-
-document.addEventListener('DOMContentLoaded', Interface.displayBooks())
+document.addEventListener('DOMContentLoaded', Interface.displayBooks());
 document.querySelector('#myform').addEventListener('submit', (e) => {
   e.preventDefault();
   const title = document.querySelector('.title').value;
   const author = document.querySelector('.author').value;
   const book = new MyBooks(title, author);
-  console.log(book);
   Interface.updateBookList(book);
   Storage.addBook(book);
   Interface.clearInputs();
 });
-
 const remove = document.querySelector('#lists');
 remove.addEventListener('click', (e) => {
-  Storage.removeBook(e.target.parentElement.previousElementSibling);
-  Interface.removeBook(e.target)
+  const title = e.target.previousElementSibling.previousElementSibling
+    .previousElementSibling.innerText;
+  const author = e.target.previousElementSibling.innerText;
+  Storage.removeBook(title, author);
+  Interface.removeBook(e.target);
 });
