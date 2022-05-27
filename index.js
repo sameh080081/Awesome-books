5b983c618d54f38d3aba901bf54814dac20a1c32
afikanzoro <muchemimutahi@gmail.com>
2022-05-27 11:40:57 +0300

    Add date function

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /* eslint max-classes-per-file: ["error", 3] */
 class Storage {
   static getBook() {
@@ -88,12 +87,31 @@ remove.addEventListener('click', (e) => {
 
 document.querySelectorAll('.nav').forEach((nav) => {
   nav.addEventListener('click', (e) => {
-   document.querySelectorAll('.nav').forEach((nav2) => nav2.classList.remove('activelink'));
+    document.querySelectorAll('.nav').forEach((nav2) => nav2.classList.remove('activelink'));
     e.preventDefault();
     e.target.classList.add('activelink');
-    document.querySelectorAll('.section').forEach((section) => {section.classList.remove('active');
-    section.classList.add('hidden')}); 
-      document.querySelector(e.target.getAttribute('href')).classList.remove('hidden')
+    document.querySelectorAll('.section').forEach((section) => {
+      section.classList.remove('active');
+      section.classList.add('hidden');
+    });
+    document.querySelector(e.target.getAttribute('href')).classList.remove('hidden');
     document.querySelector(e.target.getAttribute('href')).classList.add('active');
   });
 });
+
+const myDate = () => {
+  const date = new Date();
+  const options = {
+    weekday: undefined,
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  const [month, time] = [
+    date.toLocaleDateString(undefined, options),
+    date.toLocaleTimeString().toLocaleLowerCase(),
+  ];
+  document.getElementById('currenttime').innerHTML = `${month}, ${time}`;
+};
+myDate();
+setInterval(myDate, 1000);
